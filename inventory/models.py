e3f21bf4db766290733a025a18ab00dd48795f83
Nic-Wallace <nfewallace@gmail.com>
2023-11-19 16:16:51 +0000

    Add fields to weapon model

--- inventory/models.py
+++ inventory/models.py
@@ -27,13 +27,19 @@ class Category(models.Model):
 
 
 class Weapon(model.Model):
+    """
+    Model to create a weapon listing
+    """
     name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(max_length=1000, null=False, blank=False)
     ammunition = models.ForeignKey(Ammunition, on_delete=models.CASCADE)
     attachment = models.ForeignKey(Attachment, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     size = models.CharField(max_length=20, null=False, blank=False)
     weight = models.CharField(max_length=20, null=False, blank=False)
+    damage = models.IntegerField(null=False, blank=False)
     image = CloudinaryField("image", default="placeholder")
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ["name"]
