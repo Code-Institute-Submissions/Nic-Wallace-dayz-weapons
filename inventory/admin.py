e8d0ffeecbc6fe5a8a428cb1cb34f733a7fcc3f5
Nic-Wallace <nfewallace@gmail.com>
2023-11-22 16:40:59 +0000

    Register models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Weapon, Ammunition, Attachment, Category
 
-# Register your models here.
+admin.site.register(Ammunition)
+admin.site.register(Attachment)
+admin.site.register(Category)
+
+
+@admin.register(Weapon)
+class WeaponAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "ammunition", "attachment", "category", "size", "weight",
+        "damage", "is_public"
+    )
+    list_filter = (
+        "ammunition__name", "attachment__name", "category__name", "damage", "is_public"
+    )
