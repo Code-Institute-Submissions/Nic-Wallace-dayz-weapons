cf12162900560c98b2cab11210667617a846e31f
Nic-Wallace <nfewallace@gmail.com>
2023-11-27 21:23:45 +0000

    Create view for weapon list

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Weapon
 
-# Create your views here.
+class WeaponList(generic.ListView):
+    model = Weapon
+    queryset = Weapon.objects.filter(is_public=True).order_by('name')
+    template_name = 'index.html'
+    paginate_by = 20
