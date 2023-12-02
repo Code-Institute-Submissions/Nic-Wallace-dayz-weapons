25dac7803e135577a35e7f88c3532513d087df40
Nic-Wallace <nfewallace@gmail.com>
2023-12-02 17:39:35 +0000

    Add slug field, connect up urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.WeaponList.as_view(), name='home'),
+    path('<slug:slug>/', views.WeaponDetail.as_view(), name='weapon_detail'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Weapon
 
 class WeaponList(generic.ListView):
@@ -7,3 +7,16 @@ class WeaponList(generic.ListView):
     queryset = Weapon.objects.filter(is_public=True).order_by('name')
     template_name = 'index.html'
     paginate_by = 20
+
+
+class WeaponDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Weapon.objects.filter(is_public=True)
+        weapon = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "weapon_detail.html",
+            {"weapon": weapon,}
+        )
