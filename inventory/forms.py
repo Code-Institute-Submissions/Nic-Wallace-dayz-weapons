0641873dd7cfad75676052d4d61fc0ae0fdb8e0e
Nic-Wallace <nfewallace@gmail.com>
2023-12-08 13:49:03 +0000

    Remove slug from inventory form

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Weapon
 
+
 class WeaponForm(forms.ModelForm):
     class Meta:
         model = Weapon
-        fields = ['name', 'description', 'ammunition', 'attachment', 'category', 'size', 'weight', 'damage', 'image', 'is_public']
+        fields = [
+            'name', 'description', 'ammunition', 'attachment',
+            'category', 'size', 'weight', 'damage', 'image', 'is_public'
+        ]
