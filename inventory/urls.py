548df652e4f2cc4fb5d2697cfdac890c33696686
Nic-Wallace <nfewallace@gmail.com>
2023-12-08 13:48:19 +0000

    Split inventory URLs from the main URLs

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.WeaponList.as_view(), name='home'),
-    path('<slug:slug>/', views.WeaponDetail.as_view(), name='weapon_detail'),
+    path('add/', views.add_weapon, name='add_weapon'),
+    path('weapon/<int:id>/', views.weapon_details, name='weapon_detail'),
+    path('edit_weapon/<int:id>/', views.edit_weapon, name='edit_weapon'),
+    path('delete_weapon/<int:id>/', views.delete_weapon, name='delete_weapon'),
 ]
