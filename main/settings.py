d87d89ec5ffb3439f221e88b3761c0532532e5c4
Nic-Wallace <nfewallace@gmail.com>
2023-12-16 15:46:47 +0000

    Refactoring

--- main/settings.py
+++ main/settings.py
@@ -19,7 +19,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = development
+DEBUG = development
 DEBUG = os.environ.get('DEBUG', False)
 
 # Added CSRF_TRUSTED_ORIGINS because without it returns 403
